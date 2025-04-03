b795cdc6b64705ab81eb4cb0efa9b2050ef2d3cb
bridgeBook <k9mm10feet@yahoo.co.jp>
2025-04-03 14:15:00 +0900

    icon

--- src/App.tsx
+++ src/App.tsx
@@ -74,38 +74,67 @@ export default function AmbientMixer() {
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center">
         <div className="bg-[#F6F5F1] m-12 p-20 rounded-4xl shadow-2xl inset-shadow-sm">
 
-          <h1 className="text-xl font-bold mb-4">Ambient Sound Mixer</h1>
+          <h1 className="text-xl font-bold mb-4 text-center">Ambient Sound Mixer</h1>
 
-          <hr className="max-w-xs my-4 h-px bg-[#dddddd] border-0" />
+          <hr className="max-w my-4 h-px bg-[#dddddd] border-0" />
           <div className="flex items-center">
             {/* 鳥の音 */}
+            <img src="public\soundIcon\tori.png" className="h-12 object-scale-down" />
+            <span className="mr-4">Birds sound</span>
             <input
               type="range"
               min="0" max="1" step="0.01"
               defaultValue="0.5"
               style={backgroundStyle(birdVolume)}
-              className="w-full h-2 rounded-lg appearance-none cursor-pointer"
+              className="w-60 h-2 rounded-lg appearance-none cursor-pointer"
               onChange={(e) => { setVolume("birds", parseFloat(e.target.value)), setBirdVolume(Number(e.target.value)) }}
             />
             <button
-              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-2 ${birdTogleState ? "block" : "hidden"}`}
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${birdTogleState ? "block" : "hidden"}`}
               onClick={
                 () => { loadAndPlaySound("birds", birds, 0.5), setBirdTogleState(!birdTogleState) }
               }>
               <span className="material-icons py-2">play_arrow</span>
             </button>
             <button
-              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-2 ${birdTogleState ? "hidden" : "block"}`}
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${birdTogleState ? "hidden" : "block"}`}
               onClick={() => { stopSound("birds"), setBirdTogleState(!birdTogleState) }}
             >
               <span className="material-icons py-2">pause</span>
             </button>
           </div>
 
-          <hr className="max-w-xs my-4 h-px bg-[#dddddd] border-0" />
+          <hr className="max-w my-4 h-px bg-[#dddddd] border-0" />
+
+          <div className="flex items-center">
+            {/* 雨の音 */}
+            <img src="public\soundIcon\ame.png" className="h-12 object-scale-down mr-1" />
+            <span className="mr-4">Rain sound</span>
+            <input
+              type="range"
+              min="0" max="1" step="0.01"
+              defaultValue="0.5"
+              style={backgroundStyle(rainVolume)}
+              className="w-60 h-2 rounded-lg appearance-none cursor-pointer"
+              onChange={(e) => { setVolume("rain", parseFloat(e.target.value)), setRainVolume(Number(e.target.value)) }}
+            />
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "block" : "hidden"}`}
+              onClick={() => { loadAndPlaySound("rain", rain, 0.5), setRainTogleState(!rainTogleState) }}>
+              <span className="material-icons py-2">play_arrow</span>
+            </button>
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "hidden" : "block"}`}
+              onClick={() => { stopSound("rain"), setRainTogleState(!rainTogleState) }}
+            >
+              <span className="material-icons py-2">pause</span>
+            </button>
+          </div>
+
+          <hr className="max-w my-4 h-px bg-[#dddddd] border-0" />
 
           <div className="flex items-center">
             {/* 雨の音 */}
@@ -114,16 +143,66 @@ export default function AmbientMixer() {
               min="0" max="1" step="0.01"
               defaultValue="0.5"
               style={backgroundStyle(rainVolume)}
-              className="w-full h-2 rounded-lg appearance-none cursor-pointer"
+              className="w-60 h-2 rounded-lg appearance-none cursor-pointer"
               onChange={(e) => { setVolume("rain", parseFloat(e.target.value)), setRainVolume(Number(e.target.value)) }}
             />
             <button
-              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-2 ${rainTogleState ? "block" : "hidden"}`}
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "block" : "hidden"}`}
               onClick={() => { loadAndPlaySound("rain", rain, 0.5), setRainTogleState(!rainTogleState) }}>
               <span className="material-icons py-2">play_arrow</span>
             </button>
             <button
-              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-2 ${rainTogleState ? "hidden" : "block"}`}
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "hidden" : "block"}`}
+              onClick={() => { stopSound("rain"), setRainTogleState(!rainTogleState) }}
+            >
+              <span className="material-icons py-2">pause</span>
+            </button>
+          </div>
+
+          <hr className="max-w my-4 h-px bg-[#dddddd] border-0" />
+
+          <div className="flex items-center">
+            {/* 雨の音 */}
+            <input
+              type="range"
+              min="0" max="1" step="0.01"
+              defaultValue="0.5"
+              style={backgroundStyle(rainVolume)}
+              className="w-60 h-2 rounded-lg appearance-none cursor-pointer"
+              onChange={(e) => { setVolume("rain", parseFloat(e.target.value)), setRainVolume(Number(e.target.value)) }}
+            />
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "block" : "hidden"}`}
+              onClick={() => { loadAndPlaySound("rain", rain, 0.5), setRainTogleState(!rainTogleState) }}>
+              <span className="material-icons py-2">play_arrow</span>
+            </button>
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "hidden" : "block"}`}
+              onClick={() => { stopSound("rain"), setRainTogleState(!rainTogleState) }}
+            >
+              <span className="material-icons py-2">pause</span>
+            </button>
+          </div>
+
+          <hr className="max-w my-4 h-px bg-[#dddddd] border-0" />
+
+          <div className="flex items-center">
+            {/* 雨の音 */}
+            <input
+              type="range"
+              min="0" max="1" step="0.01"
+              defaultValue="0.5"
+              style={backgroundStyle(rainVolume)}
+              className="w-60 h-2 rounded-lg appearance-none cursor-pointer"
+              onChange={(e) => { setVolume("rain", parseFloat(e.target.value)), setRainVolume(Number(e.target.value)) }}
+            />
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "block" : "hidden"}`}
+              onClick={() => { loadAndPlaySound("rain", rain, 0.5), setRainTogleState(!rainTogleState) }}>
+              <span className="material-icons py-2">play_arrow</span>
+            </button>
+            <button
+              className={`px-4 bg-[#a9bb9a] text-white rounded-full ml-6 ${rainTogleState ? "hidden" : "block"}`}
               onClick={() => { stopSound("rain"), setRainTogleState(!rainTogleState) }}
             >
               <span className="material-icons py-2">pause</span>
